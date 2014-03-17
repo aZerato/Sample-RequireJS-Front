bb0d877684441bfe24583b17bdc65b6e456e3f4a
Jean-Phi <azerato.graphe@gmail.com>
2014-03-17 17:06:45 +0100

    Update main.js

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -2,10 +2,16 @@ require.config({
 	paths: {
 		'jquery': '../vendors/jquery',
 		'toastr': '../vendors/toastr'
+	},
+	shim: {
+		'toastr': {
+			deps: ['jquery'],
+			exports: '$'
+		}
 	}
 });
 
-require(['jquery', 'toastr'], function($, toastr) {
+require(['toastr'], function(toastr) {
 	$(document).ready(function() {
 		
 		toastr.info('Are you the 6 fingered man?');
